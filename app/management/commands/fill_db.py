2c2a06fd88f6d5cc71926b361274804651fc1059
Ivan Protopopov <154471767+Illuuusion@users.noreply.github.com>
2024-12-23 23:47:52 +0300

    Update fill_db.py

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -23,47 +23,56 @@ class Command(BaseCommand):
                 try:
                     user = User.objects.create_user(username=username)
                     users.append(user)
-                    Profile.objects.create(user=user, avatar=None)
                     break
                 except IntegrityError:
                     continue
 
-        tags = [Tag.objects.create(name=fake.word()) for _ in range(ratio)]
+        profiles = [Profile(user=user, avatar=None) for user in users]
+        Profile.objects.bulk_create(profiles)
+
+        tags = [Tag(name=fake.word()) for _ in range(ratio)]
+        Tag.objects.bulk_create(tags)
 
         questions = []
         for _ in range(ratio * 10):
-            question = Question.objects.create(
+            question = Question(
                 title=fake.sentence(),
                 text=fake.text(),
                 author=random.choice(users)
             )
-            question.tags.add(*random.sample(tags, random.randint(1, 5)))
             questions.append(question)
+        Question.objects.bulk_create(questions)
+
+        for question in questions:
+            question.tags.add(*random.sample(tags, random.randint(1, 5)))
 
         answers = []
         for _ in range(ratio * 100):
-            answer = Answer.objects.create(
+            answer = Answer(
                 question=random.choice(questions),
                 author=random.choice(users),
                 text=fake.text(),
                 is_accepted=random.choice([True, False])
             )
             answers.append(answer)
+        Answer.objects.bulk_create(answers)
 
         question_likes = []
         for _ in range(ratio * 200):
             user = random.choice(users)
             question = random.choice(questions)
             if not QuestionLike.objects.filter(user=user, question=question).exists():
-                question_like = QuestionLike.objects.create(user=user, question=question)
+                question_like = QuestionLike(user=user, question=question)
                 question_likes.append(question_like)
+        QuestionLike.objects.bulk_create(question_likes)
 
         answer_likes = []
         for _ in range(ratio * 200):
             user = random.choice(users)
             answer = random.choice(answers)
             if not AnswerLike.objects.filter(user=user, answer=answer).exists():
-                answer_like = AnswerLike.objects.create(user=user, answer=answer)
+                answer_like = AnswerLike(user=user, answer=answer)
                 answer_likes.append(answer_like)
+        AnswerLike.objects.bulk_create(answer_likes)
 
         self.stdout.write(self.style.SUCCESS(f'Successfully populated the database with a ratio of {ratio}'))
